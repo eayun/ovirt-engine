f542cf5d9fe56859b3739b54f9605f339862d85b
Dan Kenigsberg <danken@redhat.com>
2016-09-04 12:56:29 +0300

    Revert "engine: add finish report on multi-host network commands" and "core: add setCustomValues to AuditLogableBase"
    This reverts commit be69a6490f8c78887b006f94c6e42905dca54af2 and
    0475fc05e3096f73679310527d2e9ae6e351f1a1 due to reports that they cause
    failure when adding hosts to Engine.
    
    Change-Id: Icd2ce5534b8f227b9a13c70d79418d21dd5da764
    Bug-Url: https://bugzilla.redhat.com/1369064
    Signed-off-by: Dan Kenigsberg <danken@redhat.com>

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CommandBase.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/CommandBase.java
@@ -2527,11 +2527,4 @@ public abstract class CommandBase<T extends VdcActionParametersBase>
         }
 
     }
-
-    protected void setParentCommandInfo(Collection<? extends VdcActionParametersBase> parameters) {
-        for (VdcActionParametersBase param : parameters) {
-            param.setParentCommand(getActionType());
-            param.setParentParameters(getParameters());
-        }
-    }
 }
